79fc4abaf0519d361b569c4fe3bcac84e73cc299
jaro17 <jaroslav.banas@gmail.com>
2024-07-10 18:35:35 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+<!--  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>-->
+
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+
+</style>
